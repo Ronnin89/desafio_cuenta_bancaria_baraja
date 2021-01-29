2db55c7cc54b13cddbc6b78a0b1bcc92f8bc9046
Ricardo Alvarez <ri.alvarez.89@icloud.com>
2021-01-28 21:15:53 -0300

    cuentas bancarias

--- cuentas_bancarias.rb
+++ cuentas_bancarias.rb
@@ -18,14 +18,13 @@ class CuentaBancaria
       @saldo = saldo
     end
 
-    def transferir(monto, cuenta_destino, cuenta_procedencia)
-        raise ArgumentError " no tienes dinero para hacer la transferencia" if monto > cuenta_procedencia.saldo
+    def transferir(monto, cuenta_destino, cuenta_procedencia) 
         @saldo = @saldo - monto
         cuenta_destino.saldo = cuenta_destino.saldo + monto
     end
 end
 
-c1 = CuentaBancaria.new('santande', '123-4567', 10000)
-c2 = CuentaBancaria.new('Estado', '456-7891', 8000)
+c1 = CuentaBancaria.new('santande', '123-4567', 5000)
+c2 = CuentaBancaria.new('Estado', '456-7891', 5000)
 
 usuario1 = Usuario.new("ricardo", c1, c2)
